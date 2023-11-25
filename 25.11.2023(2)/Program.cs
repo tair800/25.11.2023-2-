da23fffa0743d05f6bf985c4092dbb46a3d8e9fe
tair800 <tahiraa@code.edu.az>
2023-11-25 15:29:01 +0400

    sasa

---
+++ 25.11.2023(2)/Program.cs
@@ -0,0 +1,6 @@
+﻿using _25._11._2023_2_;
+
+Student student = new ();
+
+student.FullName = "Tahir";
+student.Point = 2;
---
+++ 25.11.2023(2)/Student.cs
@@ -0,0 +1,21 @@
+﻿
+
+using System.Drawing;
+
+namespace _25._11._2023_2_
+{
+    public class Student
+    {
+        public int Id { get; } = 1;
+        public string FullName { get; set; }
+        public int Point { get; set; }
+
+    }
+    public void StudentInfo()
+    {
+        Console.WriteLine($"Student ID: {Id}");
+        Console.WriteLine($"Full Name: {FullName}");
+        Console.WriteLine($"Points: {Point}");
+        
+    }
+}
